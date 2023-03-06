84c74834b1b141160839c64b149481b77c06f221
JassiSingh08 <125473833+JassiSingh08@users.noreply.github.com>
2023-03-06 18:35:32 +0530

    Update script.js
    Added extra functions like the user can adjust speed on his own // added on-screen buttons for touch-based devices /

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const foodSound = new Audio('/music/food.mp3');
 const gameOverSound = new Audio('/music/gameover.mp3');
 const moveSound = new Audio('/music/move.mp3');
 const musicSound = new Audio('/music/music.mp3');
-let  speed = 5 ;
+let speed = 5 ; 
 let score = 0;
 let lastPaintTime = 0;
 let snakeArr = [
@@ -13,6 +13,18 @@ let snakeArr = [
 let food = {x:6, y:7};
 var hiscoreval = 0;
 
+//variables for speed buttons
+var upArr =  document.getElementById('uparrow');
+var downArr =  document.getElementById('Dnarrow');
+var dispspeed = document.getElementById('showspeed');
+var level = document.getElementById('Difficulty');
+
+//variables for onscreen buttons
+let up = document.getElementById('b1');
+let down = document.getElementById('b2');
+let left = document.getElementById('b3');
+let right = document.getElementById('b4');
+let touch =  document.getElementById('OnScreenButtons')
 
 // Game Functions
 function main(ctime){
@@ -33,12 +45,12 @@ function isCollide(snake){
         return true;
       } 
     }
+
     // if you bump into the wall
     if(snake[0].x >= 18 || snake[0].x <=0 || snake[0].y >=18 || snake[0].y <=0){
         return true;
     }
-    
-    
+        
 }
 
 function gameEngine(){
@@ -51,7 +63,11 @@ function gameEngine(){
         alert("Game Over. Your Score is " +score+ ". Press any key to play again!");
         snakeArr = [{x:13, y:15}];
         musicSound.play();
+        dispspeed.innerHTML = "Speed : " +speed ;
         score = 0;
+        scorebox.innerHTML = "Score: " +score;
+        // speed = 5 ;                                       //Everytime game over Users would not want to adjust difficulty back to at they lost 
+        // level.innerHTML = "Difficulty : Easy"            // it will go back to normal if window reloads 
     }
 
     // if food is eaten increment the score and regenerate the food 
@@ -104,7 +120,40 @@ function gameEngine(){
     foodElement.style.gridColumnStart =  food.x;  // to place food in the x column
     foodElement.classList.add('food')
     board.appendChild(foodElement);
+
+ 
 }
+
+// Difficulty/  let the user update it USING ONSCREEN BUTTONS
+
+upArr.addEventListener("click",()=>{
+    speed += 1;
+    dispspeed.innerHTML = "Speed : " +speed ;
+    if(speed > 14)                                  // Added difficulty Indicator for the user 
+    { 
+        level.innerHTML = "Difficulty : Medium";
+    }
+    if( speed > 29)
+    {
+        level.innerHTML = "Difficulty : Hard";
+        console.log("speed")
+    }
+  
+    // console.log("new speed " +speed);
+})
+
+downArr.addEventListener("click",()=>{
+    speed -= 1;
+    if(speed<0){
+        alert("Speed Can Not Be Negative!");
+        speed = 1 
+    }
+    dispspeed.innerHTML = "Speed : " +speed ;
+
+    // console.log("new speed " +speed);
+})
+
+
 
 
 
@@ -123,7 +172,7 @@ else{
 
 window.requestAnimationFrame(main);
 window.addEventListener('keydown', e =>{
-    inputDir = {x: 0, y: 1} // Start the game
+    inputDir = {x: 0, y: -1} // Start the game
     moveSound.play();
     switch (e.key) {
         case "ArrowUp":
@@ -152,5 +201,30 @@ window.addEventListener('keydown', e =>{
         default:
             break;
     }
-
+   
 });
+
+// ON SCREEN BUTTONS
+
+OnScreenButtons.addEventListener('mouseover', () =>{   //when mouse is over onscreen buttons are active and can be used
+    up.addEventListener("click",()=>{
+        console.log("ArrowUp");
+        inputDir.x = 0;
+        inputDir.y = -1;
+    })
+    down.addEventListener("click",()=>{
+        console.log("ArrowDown");
+        inputDir.x = 0;
+        inputDir.y = 1;
+    })
+    left.addEventListener("click",()=>{
+        console.log("ArrowLeft");
+        inputDir.x = -1;
+        inputDir.y = 0;
+    })
+    right.addEventListener("click",()=>{
+        console.log("ArrowRight");
+        inputDir.x = 1;
+        inputDir.y = 0;
+    })
+    });
